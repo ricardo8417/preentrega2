2688f6837eec1e9686db675ce7d47356e22faf29
Ricardo <rnavarrete_cervantes@hotmail.com>
2023-02-17 22:30:33 -0600

    Segunda pre-entrega

---
+++ main.js
@@ -0,0 +1,104 @@
+const inventario = []
+
+
+const bienvenida = () => {
+    alert("Bienvenido al Simulador de Inventarios")
+
+    menuInventario()
+
+}
+
+
+
+
+//Variables Globales
+const menuInventario = () => {
+
+        let pregunta = 0;
+        let totalProductoAlta = 0
+        let totalProductoBaja = 0
+
+
+        do {
+
+            pregunta = parseInt(prompt("Presione la opción de la operación que desea realizar: 1) Alta de producto, 2) Baja del producto, 3) Detalle de existencias del producto, 4)Salir"))
+            if (pregunta == 1) {
+                ordenarAlfabeticamente()
+                agregarArticulos(listaStock);
+                alert('Productos o Producto ingresado con exito.');
+            } else if (pregunta == 2) {
+                darBajaProducto()
+                alert('Productos o Producto dados de baja con exito.');
+            } else if (pregunta == 3) {
+                existencia(totalProductoAlta, totalProductoBaja)
+
+            } else if (pregunta == 4) {
+                pregunta = false;
+                alert('Ha seleccionado salir.')
+            } else {
+                alert('Error!, Ingreso una opción incorrecta, favor de seleccionar la opción correcta.');
+            }
+        } while (pregunta);
+    }
+    //función dar alta al producto
+
+const ordenarAlfabeticamente = () => {
+    stock.sort((a) => a.nombre)
+    mostrarStock()
+}
+
+const mostrarStock = () => {
+    const listaStock = stock.map(producto => {
+        return '-' + producto.nombre
+    })
+    alert('Lista de Productos para agregar al Stock del inventario:' + '\n\n' + listaStock.join('\n'))
+    agregarArticulos(listaStock)
+}
+
+
+const agregarArticulos = (listaStock) => {
+    let cantidad = 0
+    let totalProductoAlta = 0
+
+    do {
+        producto = prompt('¿Que producto deseas dar de alta:' + '\n\n' + listaStock.join('\n'))
+        cantidad = parseInt(prompt('Agrega la cantidad del producto a dar de alta'))
+        anaquel = prompt('Escribe el anaquel al que corresponde tu producto, ejemplo: Limpieza,moviliario, Computo o Papeleria'.toLowerCase())
+
+        totalProductoAlta += cantidad
+        agregarProducto = confirm('¿Desea dar de alta otro producto?')
+    } while (agregarProducto);
+    return totalProductoAlta
+
+};
+
+
+//función dar baja al producto
+
+function darBajaProducto() {
+    let cantidadBaja = 0
+    let totalProductoBaja = 0
+    let continuarBaja = false
+
+    do {
+        productoBaja = prompt(' Selecciona el producto que deseas dar de alta, cátalogo: Escoba, Plumones,Computadora,Escritorios '.toLowerCase())
+        cantidadBaja = parseInt(prompt('Cantidad de producto a utilizar'))
+        totalProductoBaja += cantidadBaja
+        continuarBaja = confirm('Desea utilizar otro producto')
+
+    } while (continuarBaja)
+    return totalProductoBaja
+};
+
+//función calculo existencias
+
+function existencia(totalProductoAlta, totalProductoBaja) {
+    let existencias = 0
+
+
+    existencias = totalProductoAlta - totalProductoBaja;
+
+    alert('Actualmente cuentas con: ' + existencias)
+
+};
+bienvenida()
---
+++ productos.js
@@ -0,0 +1,109 @@
+const stock = [{
+        id: 1,
+        nombre: "Plumones",
+        tipo: "Marcadores",
+        anaquel: "Papeleria",
+        precio: 52,
+        cantidad: 0
+
+    },
+    {
+        id: 2,
+        nombre: "Silla",
+        tipo: "Silla Secretarial",
+        anaquel: "Mobiliario",
+        precio: 2800,
+        cantidad: 0
+
+    },
+    {
+        id: 3,
+        nombre: "Teclado",
+        tipo: "Hardware",
+        anaquel: "Computo",
+        precio: 250,
+        cantidad: 0
+
+    },
+    {
+        id: 4,
+        nombre: "Pinol",
+        tipo: "Limpiador de piso",
+        anaquel: "Limpieza",
+        precio: 360,
+        cantidad: 0
+
+    },
+    {
+        id: 5,
+        nombre: "Mouse",
+        tipo: "Hardware",
+        anaquel: "Computo",
+        precio: 250,
+        cantidad: 0
+
+    },
+    {
+        id: 6,
+        nombre: "Mesas",
+        tipo: "Mesas de Escritorio",
+        anaquel: "Mobiliario",
+        precio: 5500,
+        cantidad: 0
+
+    },
+    {
+        id: 7,
+        nombre: "Hojas Blancas",
+        tipo: "Hojas de impresión",
+        anaquel: "Papeleria",
+        precio: 500,
+        cantidad: 0
+
+    },
+    {
+        id: 8,
+        nombre: "Trapeador",
+        tipo: "Articulo de Limpieza",
+        anaquel: "Limpieza",
+        precio: 500,
+        cantidad: 0
+
+    },
+    {
+        id: 9,
+        nombre: "Disco Duro",
+        tipo: "Hardware",
+        anaquel: "Computo",
+        precio: 2000,
+        cantidad: 0
+
+    },
+    {
+        id: 10,
+        nombre: "Archivero",
+        tipo: "Mueble para Archivar Documentos",
+        anaquel: "Mobiliario",
+        precio: 4200,
+        cantidad: 0
+
+    },
+    {
+        id: 11,
+        nombre: "Clips",
+        tipo: "Caja de Clips",
+        anaquel: "Papeleria",
+        precio: 150,
+        cantidad: 0
+
+    },
+    {
+        id: 12,
+        nombre: "Escoba",
+        tipo: "Articulo de Limpieza",
+        anaquel: "Limpieza",
+        precio: 500,
+        cantidad: 0
+
+    }
+]
